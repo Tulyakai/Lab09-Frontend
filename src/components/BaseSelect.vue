9dd0767c1c72598c79cfb176f2792d90bf6f67e3
Tulyakai <tulyawatt@gmail.com>
2021-09-09 03:11:48 +0700

    6.6

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <label v-if="label">{{ label }}</label>
+        <select
+            class="field"
+            :value="modelValue"
+            v-bind="{
+                ...$args, 
+                onChange: ($event) => {$emit('update:modelValue', $event.target.value)}
+            }">
+            <option v-for="option in options" :value="option.id" :key="option.id" :selected="option.id === modelValue.id">{{option.name}}</option>
+        </select>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        label: {
+            type:String,
+            default: ''
+            
+        },
+        modelValue: {
+            type: [String, Number, Object],
+            default: ''
+        },
+        options: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
